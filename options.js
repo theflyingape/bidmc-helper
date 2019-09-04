adbb4c5d0d1920dc690d70fca132722b5ef50553
Robert Hurst <rhurst@8ball.bidmc.harvard.edu>
2019-09-04 10:45:54 -0400

    redo buttons

--- options.js
+++ options.js
@@ -3,27 +3,20 @@ const url = chrome.runtime.getURL("assets/config.json");
 fetch(url).then((response) => response.json().then((config) => {
     chrome.storage.managed.get(function(policy) {
         console.log('JSON configuration options: ', JSON.stringify(policy));
+        //  compare config's built-in features with the loaded extension policy
+        let app = chrome.runtime.getManifest();
+        let features = document.getElementById('features');
+        features.innerHTML = `<h2 style="color: darkslateblue;">v${app.version} enabled features</h2><ul>`;
+        for (i in config.features)
+            if (policy[i]) features.innerHTML += `<li>${i}: ${config.features[i].text}</li>`;
+        features.innerHTML += '</ul>';
         //  enumerate any managed buttons first
         if (policy.buttons && policy.buttons.length) {
             let buttons = document.getElementById('buttons');
             for (i in policy.buttons) {
                 buttons.innerHTML +=
-                    `<p><button id="button${i}">${policy.buttons[i].caption}</button></p>`;
-            }
-        //  soft-create: chrome.tabs.create({ url: 'https://edmenu.bidmc.harvard.edu/' });
-            for (i in policy.buttons) {
-                document.getElementById(`button${i}`)
-                    .addEventListener('click', `window.open('${policy.buttons[i].url}', '_blank');`);
+                    `<p><button onclick="chrome.tabs.create({ url:'${policy.buttons[i].url}' });">${policy.buttons[i].caption}</button></p>`;
             }
         }
-
-        //  compare config's built-in features with the loaded extension policy
-        let app = chrome.runtime.getManifest();
-        let features = document.getElementById('features');
-        features.innerHTML = `<h2 style="color: darkslateblue;">v${app.version} enabled features</h2><ul>`;
-        for (i in config.features) {
-            if (policy[i]) features.innerHTML += `<li>${i}: ${config.features[i].text}</li>`;
-        }
-        features.innerHTML += '</ul>';
     });
 }));
